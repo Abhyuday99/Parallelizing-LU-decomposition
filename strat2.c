d81a69ea0ecd3ef9ed125699877fc6e4959fc58c
Abhyuday99 <abhyuday99@LAPTOP-UJNKDODD.localdomain>
2021-04-20 19:03:52 +0530

    Add tests and strat2

--- strat2.c
+++ strat2.c
@@ -11,25 +11,34 @@ void crout(double const **A, double **L, double **U, int n) {
 	for (j = 0; j < n; j++) {
 		sum = 0;
 		for (k = 0; k < j; k++) {
-			sum = sum + L[i][k] * U[k][j];	
+			sum = sum + L[j][k] * U[k][j];	
 		}
-		L[i][j] = A[i][j] - sum;
-		for (i = j; i < n; i++) {
-			sum = 0;
-			for (k = 0; k < j; k++) {
-				sum = sum + L[i][k] * U[k][j];	
+		L[j][j] = A[j][j] - sum;
+		#pragma omp parallel sections private(sum,i,k) shared(j)
+		{	
+			#pragma omp section
+			{
+				for (i = j + 1; i < n; i++) {
+					sum = 0;
+					for (k = 0; k < j; k++) {
+						sum = sum + L[i][k] * U[k][j];	
+					}
+					L[i][j] = A[i][j] - sum;
+				}
 			}
-			L[i][j] = A[i][j] - sum;
-		}
-		for (i = j; i < n; i++) {
-			sum = 0;
-			for(k = 0; k < j; k++) {
-				sum = sum + L[j][k] * U[k][i];
+			#pragma omp section
+			{
+				for (i = j; i < n; i++) {
+					sum = 0;
+					for(k = 0; k < j; k++) {
+						sum = sum + L[j][k] * U[k][i];
+					}
+					if (L[j][j] == 0) {				
+						exit(0);
+					}
+					U[j][i] = (A[j][i] - sum) / L[j][j];
+				}
 			}
-			if (L[j][j] == 0) {				
-				exit(0);
-			}
-			U[j][i] = (A[j][i] - sum) / L[j][j];
 		}
 	}
 }
